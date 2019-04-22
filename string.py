3bdc66b91608902365434e93cf815fe2fe72c404
Volin <j_yuefeng@163.com>
2019-04-22 23:51:24 +0800

    testString:主要测试了判断是否是数字

--- string.py
+++ string.py
@@ -42,3 +42,34 @@ print('-12.56'.isdigit())
 print('--------isdigit:12abcd--------')
 print('12abcd'.isdigit())
 print('12abcd'.isalnum())
+
+
+def is_number(s):
+    try:  # 如果能运行float(s)语句，返回True（字符串s是浮点数）
+        float(s)
+        return True
+    except ValueError:  # ValueError为Python的一种标准异常，表示"传入无效的参数"
+        pass  # 如果引发了ValueError这种异常，不做任何事情（pass：不做任何事情，一般用做占位语句）
+    try:
+        import unicodedata  # 处理ASCii码的包
+        unicodedata.numeric(s)  # 把一个表示数字的字符串转换为浮点数返回的函数
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
+print('----------123:')
+print(is_number('123'))
+print('----------abcd:')
+print(is_number('abcd'))
+print('----------123abcd:')
+print(is_number('123adcd'))
+print('---------- -123:')
+print(is_number('-123'))
+print('----------1.234:')
+print(is_number('1.234'))
+print('---------- -1.234:')
+print(is_number('-1.234'))
+print('----------0:')
+print(is_number('0'))
+a="   sdads\r\n"
+print(a.strip())
