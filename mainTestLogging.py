560253e6cc55c1fd297fa419e52232289e965cc0
Volin <j_yuefeng@163.com>
2019-05-31 23:18:02 +0800

    test pep8

--- mainTestLogging.py
+++ mainTestLogging.py
@@ -2,19 +2,18 @@ import logging
 import os
 import testLogging1
 import testLogging
-thisPath=os.path.dirname(os.path.realpath(__file__))
+thisPath = os.path.dirname(os.path.realpath(__file__))
 print(thisPath)
 logging.basicConfig(level=logging.DEBUG,
-                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-                datefmt='%a, %d %b %Y %H:%M:%S',
-                filename='myapp.log',
-                filemode='w')
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='myapp.log',
+                    filemode='w')
 logging.info('main')
-t1=testLogging1.test1()
-t2=testLogging.test2()
+t1 = testLogging1.test1()
+t2 = testLogging.test2()
 t1.testA()
 t2.test2A()
 t1.testA()
 t2.test2A()
 logging.info('3333')
-
---
+++ pygameMovie.py
@@ -0,0 +1,3 @@
+from pygame import movie
+movie.name
+# 此模块不太好用，早已废弃
