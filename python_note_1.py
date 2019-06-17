ff7f63b7f877436eaf233b2fcd54a7422f132079
Volin <j_yuefeng@163.com>
2019-06-17 23:59:47 +0800

    add python note

---
+++ python_note_1.py
@@ -0,0 +1,19 @@
+# *相乘，用在字符串时表示重复
+print('a' * 25)
+
+# 命令行执行python程序的方式之一：
+# % python script.py > d:\1.txt
+
+# input() 等待输入
+
+# 每一个.py结尾的python源代码文件都是一个python模块
+
+# 重载模块 imp.reload(mode_a)
+
+# dir 获得模块内部的可用全部变量名的列表
+# help 查看文档注释
+
+dir(dict)
+help(dict)
+
+
---
+++ python_note_2.py
@@ -0,0 +1,26 @@
+# 核心数据类型
+
+# 数字 1234，3.14，3+4j,0b111,Decimal(),Fraction()
+# 字符串 
+# 列表
+# 字典
+# 元组
+# 文件
+# 集合 set('abc'), {'a','b','c'}
+# 类型
+# None
+# 布尔型
+# 函数
+# 模块
+# 类
+# Python实现相关类型： 已编译代码、调用栈跟踪
+
+# 数学模块
+import math
+print('math.pi: ', math.pi)
+print('math.sqrt(85): ', math.sqrt(85))
+
+# 随机模块
+import random
+print('random.random(): ', random.random())
+print('random.choice([1,2,3,4]): ', random.choice([1,2,3,4]))
