a301b8f0a1f730011edaa0c0e4afc484e9962be6
Volin <j_yuefeng@163.com>
2019-08-04 12:15:42 +0800

    增加ai学院的加解密作业

---
+++ ai_learning/secret_1.py
@@ -0,0 +1,60 @@
+def encode_(text):
+    '''加密汉字和英文字母部分
+    '''
+    new_text = ''
+    new_key = []
+    global key
+    if isinstance(text, str):
+        text_list = list(text)
+    else:
+        return text
+    for i, char in enumerate(text_list):
+        if is_eng(char) or is_zh(char):
+            text_list[i] = chr(ord(char) + 900000)
+    for i in text_list:
+        new_text += i
+    return new_text
+
+
+def decode_(text):
+    '''解密
+    '''
+    new_text = ''
+    text_list = list(text)
+    for i, char in enumerate(text_list):
+        if ord(char) > 900000:
+            text_list[i] = chr(ord(char)-900000)
+    for i in text_list:
+        new_text += i
+    return new_text
+
+
+def is_zh(uchar):
+    '''判断是否是中文汉字
+    args:
+        uchar: unicode编码的字符
+    '''
+    if uchar >= u'\u4E00' and uchar <= u'\u9FA5':
+        return True
+    else:
+        return False
+
+
+def is_eng(uchar):
+    '''判断是否是英文字母
+    args：
+        uchar:unicode编码的字符
+    '''
+    if (uchar >= u'\u0041' and uchar <= u'\u005A'):
+        return True
+    elif (uchar >= u'\u0061' and uchar <= u'\u007A'):
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    a = encode_('ab')
+    print(a)
+    a = decode_(a)
+    print(a)
---
+++ pyqt_notes/note1.py
@@ -0,0 +1,9 @@
+# # # Tkinter,使用tcl实现
+# # TK 易学易用，方便创建简单GUI，跨平台。
+# # 不足：全靠代码实现，简陋
+# # Wxpython， 文档少，遇到问题不容易解决，代码布局控件，不直观
+# pygtk linux下很完美，但在windows下兼容性有问题
+
+# pyqt c++， qt是Nokia的， GPL协议，商业程序需要购买商业版授权
+
+# pyqt很容易迁移到pyside
